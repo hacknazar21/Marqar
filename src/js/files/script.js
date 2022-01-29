d96462bc0c35610581865f5f97ae0116caace2d2
Nazar Kultayev <nazarcool2001@gmail.com>
2022-01-29 12:18:05 +0500

    Сделал шапку, первый экран, карточки с кейсами

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,4 +3,30 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-
+function cutLongText() {
+    var elem, size, text;
+    elem = document.getElementsByClassName('case-card__text');
+    size = 900; // Количество отображаемых символов
+    for (let element of elem) {
+        text = element.innerHTML;
+        if (text.length > size) {
+            text = text.slice(0, size);
+        }
+        element.innerHTML = text + '...';
+    }
+}
+function ListenBtnMenu() {
+    const buttonsMenu = document.getElementsByClassName('menu_arrow');
+    let flag = false;
+    for (let buttonMenu of buttonsMenu) {
+        console.log(buttonMenu);
+        buttonMenu.addEventListener('click', (event) => {
+            event.preventDefault();
+            if (!flag) event.target.classList.add("_active");
+            else event.target.classList.remove("_active");
+            flag = !flag;
+        });
+    }
+}
+ListenBtnMenu();
+cutLongText();
